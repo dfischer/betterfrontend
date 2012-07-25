1b582acd79c8ee24352f80450b485cb26428da52
DFischer <me@danielfischer.com>
2012-07-24 17:47:22 -0700

    Attempt to get a site up

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source :rubygems
+
+gem 'nanoc'
+gem 'kramdown'
+gem 'adsf'
+gem 'mime-types'
+gem 'sass'
+gem 'haml'
+gem 'compass'
+gem 'zurb-foundation', '~> 3.0.6'
+
+gem 'rack-contrib'
+gem 'heroku'
---
+++ site/compass/config.rb
@@ -0,0 +1,12 @@
+require 'zurb-foundation'
+
+http_path    = "/" 
+project_path = "." 
+css_dir      = "output/stylesheets/" 
+sass_dir     = "content/stylesheets/" 
+images_dir   = "output/images/"
+
+# when using SCSS:
+sass_options = {
+  :syntax => :scss
+}
